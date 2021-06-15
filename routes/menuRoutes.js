bb855a9b6cc199c7b48abfa7a082ccc0165ddaf1
Nico <Nico@MacBook-Pro-de-Nico.local>
2021-06-15 11:49:28 -0300

    tercer commit

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,3 +1,8 @@
 const express = require('express')
 const menuRoutes = express.Router()
 
+const menuController = require('../controllers/menuController')
+
+menuRoutes.get('/list', menuController.list)
+
+module.exports = menuRoutes
